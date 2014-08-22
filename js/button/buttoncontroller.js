b4e5943c4c05e52b21a40b52a4c5c5ff11059152
nehachaudhary <ch.neha89@gmail.com>
2014-08-22 17:58:46 +0530

    Update ButtonController
    Button Controller should not have any information related to calculator as it is independent module.

--- js/button/buttoncontroller.js
+++ js/button/buttoncontroller.js
@@ -1,10 +1,10 @@
 (function () {
-    'us strict';
+    'use strict';
 
     function buttonController($scope, buttonData) {
         $scope.clickedButtonData = buttonData;
         $scope.buttonClicked = function (data) {
-            $scope.calculatorData.clickedButtonData = data;
+            $scope.buttonClickedData = data;
         };
     }
 
